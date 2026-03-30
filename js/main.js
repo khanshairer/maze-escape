9c4af749f47b86f695783f8f977d368293a9127e
Sharier Khan <mrkhansharier@gmail.com>
2026-03-30 15:47:12 -0230

    added ground attackers and collsion detection and jump features for main characters

--- js/main.js
+++ js/main.js
@@ -1,12 +1,15 @@
 import { World } from './World.js';
+import { Game } from './gameLogic/game.js';
 
 const world = new World();
 world.init();
+const game = new Game(world);
 
 // Animate loop
 function loop() {
   requestAnimationFrame(loop);
-
+  
+  game.update();
   world.update();
   world.render();
 }
